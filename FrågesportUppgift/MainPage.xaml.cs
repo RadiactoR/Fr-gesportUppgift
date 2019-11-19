67855daa6da3ab62bb0cf5ff9225837cbdf18a71
Weeaboo420 <rhg2005@hotmail.com>
2019-11-19 09:16:47 +0100

    gkjrgljaweklfawf

--- FrågesportUppgift/MainPage.xaml.cs
+++ FrågesportUppgift/MainPage.xaml.cs
@@ -24,15 +24,81 @@ namespace FrågesportUppgift
     {
         private Manager manager;
         private List<Question> questions;
+        private int correctAnswers = 0;
+        private Question currentQuestion;
+        private Random rand = new Random();
+        private int noQuestions = 2;
 
         public MainPage()
         {
             this.InitializeComponent();
             questions = new List<Question>();
-            questions.Add(new Question("En testfråga", new List<string> { "a", "b", "c"}, 0));
 
-            qBox.Text = questions[0].GetText;
+            questions.Add(new Question("Vad heter Sveriges huvudstad?", new List<string> { "Oslo", "Stockholm", "Köpenhamn"}, 1));
+            questions.Add(new Question("Vad heter Norges huvudstad?", new List<string> { "Stockholm", "Storbritannien", "Oslo" }, 2));
 
+            //Applicera
+            PrintQuestion(questions[rand.Next(0, questions.Count)]);
+        }
+
+
+
+        private void GetNextQuestion()
+        {
+            questions.Remove(currentQuestion);
+            if (questions.Count >= 1)
+            {
+                PrintQuestion(questions[rand.Next(0, questions.Count)]);
+            }
+            else
+            {
+                qBox.Text = "Tack för att du spelade!";
+                scoreBox.Text = "";
+            }
+        }
+
+
+        private void Answer(int i)
+        {
+            if (currentQuestion.isCorrect(i))
+            {
+                //Ny fråga
+                correctAnswers += 1;
+                GetNextQuestion();
+            }
+            else
+            {
+                //Ny fråga
+                GetNextQuestion();
+            }
+        }
+
+        private void PrintQuestion(Question q)
+        {
+            currentQuestion = q;
+            scoreBox.Text = correctAnswers.ToString() + "/" + noQuestions.ToString() + " poäng";
+            qBox.Text = q.GetText;
+            a1.Content = q.GetAnswers[0];
+            a2.Content = q.GetAnswers[1];
+            a3.Content = q.GetAnswers[2];
+        }
+
+
+
+
+        private void a1_Click(object sender, RoutedEventArgs e)
+        {
+            Answer(0);
+        }
+
+        private void a2_Click(object sender, RoutedEventArgs e)
+        {
+            Answer(1);
+        }
+
+        private void a3_Click(object sender, RoutedEventArgs e)
+        {
+            Answer(2);
         }
     }
 }
